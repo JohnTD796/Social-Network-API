9499e8ba504327156009321c912f93e8d08ba2d8
JohnTD796 <156860528+JohnTD796@users.noreply.github.com>
2024-07-03 13:57:05 -0400

    fixed get single user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,7 +17,7 @@ module.exports = {
     try {
       const user = await User.findOne({ _id: req.params.userId })
         .select('-__v')
-        .populate('thoughts', 'friends');
+        .populate('thoughts');
 
       if (!user) {
         return res.status(404).json({ message: 'No user with that ID' });
@@ -25,6 +25,7 @@ module.exports = {
 
       res.json(user);
     } catch (err) {
+      console.log(err)
       res.status(500).json(err);
     }
   },
